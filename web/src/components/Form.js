c5044fdc2e4215ef9a3ad132fb327dec40417a91
Tony Heimark <tjh-94@hotmail.com>
2019-07-06 20:50:32 +0200

    contact form

---
+++ web/src/components/Form.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+export default props => {
+  const [active, setActive] = useState(false);
+  const [activeTwo, setActiveTwo] = useState(false);
+  const [activeThree, setActiveThree] = useState(false);
+
+  const toggleActive = () => {
+    if (active === false) {
+      setActive(true);
+    }
+  };
+
+  const removeActive = e => {
+    if (!e.target.value && active) {
+      setActive(false);
+    }
+  };
+
+  const toggleActiveTwo = () => {
+    if (activeTwo === false) {
+      setActiveTwo(true);
+    }
+  };
+
+  const removeActiveTwo = e => {
+    if (!e.target.value && activeTwo) {
+      setActiveTwo(false);
+    }
+  };
+
+  const toggleActiveThree = () => {
+    if (activeThree === false) {
+      setActiveThree(true);
+    }
+  };
+
+  const removeActiveThree = e => {
+    if (!e.target.value && activeThree) {
+      setActiveThree(false);
+    }
+  };
+
+  return (
+    <form name="contact" method="post" autoComplete="off">
+      <input type="hidden" name="form-name" value="contact" autoComplete="false" />
+      <p>
+        <label className="form-label-name">
+          Your Name:{" "}
+          <input
+            onFocus={toggleActive}
+            onBlur={removeActive}
+            className={active ? "Active" : "Inactive"}
+            type="text"
+            name="name"
+          />
+        </label>
+      </p>
+      <p>
+        <label className="form-label">
+          Your Email:{" "}
+          <input
+            onFocus={toggleActiveTwo}
+            onBlur={removeActiveTwo}
+            className={activeTwo ? "Active" : "Inactive"}
+            type="email"
+            name="email"
+          />
+        </label>
+      </p>
+      <p>
+        <label className="form-label-message">
+          Message:{" "}
+          <textarea
+            onFocus={toggleActiveThree}
+            onBlur={removeActiveThree}
+            className={activeThree ? "ActiveField" : "Inactive"}
+            name="message"
+          />
+        </label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+  );
+};
--- web/src/pages/contact.js
+++ web/src/pages/contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Form from "../components/Form";
 
 import Layout from "../components/layout";
 import Container from "../components/container";
@@ -11,7 +12,22 @@ const ContactPage = () => (
     <SEO title="Contact" />
     <Container>
       <h1 className={responsiveTitle1}>Contact</h1>
-      <p>Contact me</p>
+      <div className="contact-text">
+        <h2>Contact Me</h2>
+        <p>Phone: +47 41788033</p>
+        <p>
+          Email: <a href="mailto:tjh@hotmail.com">Tjh@hotmail.com</a>
+        </p>
+        <a href="https://www.linkedin.com/in/tony-j-heimark/">
+          <i className="fab fa-linkedin" />
+        </a>
+        <a href="https://github.com/TonyHeimark">
+          <i className="fab fa-github-square" />
+        </a>
+      </div>
+      <div className="contact-form-container">
+        <Form />
+      </div>
     </Container>
   </Layout>
 );
